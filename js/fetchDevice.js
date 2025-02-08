7b6dfb01727403a7d632b844ed4d86e9f3275a51
Trần Quốc Huy <huytranquoc298@gmail.com>
2025-02-08 16:23:40 +0700

    update

--- js/fetchDevice.js
+++ js/fetchDevice.js
@@ -1,4 +1,7 @@
-var devicesChoose = null;
+let xArray = [];
+let yArray = [];
+const timeIntervals = 500;
+
 async function fecthDevice(apiUrl) {
     try{
         const response = await fetch(apiUrl);
@@ -60,8 +63,10 @@ async function getDevice(apiUrl, id){
             console.warn("API Error:", data.error);
             return null;
         }
-        devicesChoose = id;
-        drawData();
+        //set data null
+        xArray = [];
+        yArray = [];
+        
         const devices = Array.isArray(data) ? data : [data];
         devices.forEach(device => {
             let num = device.deviceID;
@@ -71,24 +76,24 @@ async function getDevice(apiUrl, id){
             
         });
         console.log("Fetched devices:", devices);
-        return data;
-        
+        //call function then set time to reapeatlly call api to refresh table
+        sendApiRequest('http://localhost:5000/record?deviceID=', id);
+        //setTimeout(() => getData('http://localhost:5000/record?deviceID=', id), timeIntervals);
+
+
     }catch(error){
         console.error("Error fetching data: ", error);
         console.error(apiUrl);
     }
 }
-async function drawData() {
-    if(devicesChoose === null){
-        return null;
-    }
-    const xArray = [50,60,70,80,90,100,110,120,130,140,150];
-    const yArray = [7,8,8,9,9,9,10,11,14,14,15];
+async function resetTable() {
+    xArray = [];
+    yArray = [];
         
         // Define Data
     const data = [{
-        x: ['2024-10-04 22:23:00', '2024-11-04 22:23:00', '2024-12-04 22:23:00'],
-        y:[1, 3, 6],
+        x:xArray,
+        y:yArray,
         type: 'scatter',
         marker: {
             color: 'blue',
@@ -101,9 +106,7 @@ async function drawData() {
     const layout = {
       xaxis: {range: [40, 160], title: "Time"},
       yaxis: {range: [5, 16], title: "uSv"},  
-      font :{
-        family : './font/Bai_Jamjuree/BaiJamjuree-Regular.ttf',
-      },
+      
       title: "Data table",
         paper_bgcolor: 'rgba(0,0,0,0)',
         plot_bgcolor: 'rgba(0,0,0,0)',
@@ -112,6 +115,81 @@ async function drawData() {
     // Display using Plotly
     Plotly.newPlot("myPlot", data, layout,  {scrollZoom: true});
 }
-async function getData(apiUrl){
+async function getData(apiUrl, id){
     
+    try {
+        apiUrl += id;
+        const response = await fetch(apiUrl);
+        if(!response.ok){
+            throw new Error('Http error! Status : ${response.status}$');
+        }
+        const data = await response.json();
+        if(data.error){
+            console.warn("API Error:", data.error);
+            return null;
+        }
+        
+        const devices = Array.isArray(data) ? data : [data];
+
+        if(yArray.length > 0){
+            if(Date(devices[devices.length - 1].timeStamp) === Date(yArray[yArray.length - 1])){
+                console.warn(yArray.length);
+                return null;
+            }
+            return devices;
+        }
+        devices.forEach(device => {
+            const myUnixTimestamp = device.timeStamp; // start with a Unix timestamp
+
+            const myDate = new Date(myUnixTimestamp * 1000); // convert timestamp to milliseconds and construct Date object
+            if(yArray.length > 0 && Date(yArray[yArray.length - 1]) > myDate){
+                return;
+            }
+            xArray.push(myDate); 
+            yArray.push(parseInt(device.Cps).toString());
+        });
+        const dataDevice = [{
+            x:xArray,
+            y:yArray,
+            type: 'scatter',
+            marker: {
+                color: 'blue',
+                    //background color of the chart container space
+                    //background color of plot area
+            }
+        }];
+        // Define Layout
+        const layout = {
+            xaxis: {range: [40, 160], title: "Time"},
+            yaxis: {range: [5, 16], title: "uSv"},  
+            
+            title: "Data table",
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
+        };
+        
+        // Display using Plotly
+        Plotly.newPlot("myPlot", dataDevice, layout,  {scrollZoom: true});
+
+        console.log("Fetched devices:", devices);
+        return data;
+        
+    }catch(error){
+        console.error("Error fetching data: ", error);
+        console.error(apiUrl);
+        resetTable();
+    }
+}
+function sendApiRequest(apiUrl, id) {
+    getData(apiUrl, id).finally(() => {
+        setTimeout(() => sendApiRequest(apiUrl, id), timeIntervals);
+    });
+}
+
+function sendApiRequest_nonRecursion(apiUrl, id){
+    function createIntervals(){
+        function run(){
+            //getData(apiUrl, id).then(() )
+        }
+    }
 }
