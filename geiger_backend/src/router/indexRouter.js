d55b975ec212a3b2512329b774d8f39f9c7d7119
huytran298 <huytranquoc298@gmail.com>
2025-02-21 21:10:37 +0700

    update ignore

--- geiger_backend/src/router/indexRouter.js
+++
@@ -1,8 +0,0 @@
-const deviceRoutes = require("./deviceRouter");
-const recordRoutes = require("./recordRouter");
-
-module.exports = (app) => {
-    app.use('/', deviceRoutes)
-    app.use('/', recordRoutes)
-}
-
