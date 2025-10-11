f32cd2adff03c50dcc392c79865494dc43637876
LeThinh01 <thinhthcsah11@gmail.com>
2025-10-11 14:12:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors());
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'src', 'public')));
+app.use(express.static(path.join(__dirname))); // Serve static files from the root directory
 
 // Database connection
 const db = createDBconnection(path.join(__dirname, 'database', 'geiger.db'));
